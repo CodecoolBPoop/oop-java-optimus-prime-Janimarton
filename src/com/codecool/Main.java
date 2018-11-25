01935fe16d67919e481e3ffdf617b99fb52b44a7
Jani <janimarton22@gmail.com>
2018-11-25 17:24:23 +0100

    the program can find all prim numbers in the examined interval

--- src/com/codecool/Main.java
+++ src/com/codecool/Main.java
@@ -3,6 +3,16 @@ package com.codecool;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int examinedInterval = 100;
+        boolean[] primArray = new boolean[examinedInterval];
+
+        for (int i = 2; i < (Math.sqrt(primArray.length) + 1); i++) {
+            for (int j = i * i; j < examinedInterval; j = j + i) {
+                primArray[j] = true;
+
+            }
+        }
+        for (int i = 2; i < primArray.length; i++) if (!primArray[i]) System.out.print(i + "  ");
+
     }
 }
